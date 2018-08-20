5ccd54a91c5aaf1e4b7bcc6c711f4ba3f1382b43
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 02:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
     for (var cart.length; i++) {
       var obj = cart[i];
       cart_str += 'In your cart, you have ' + obj[itemName] + ' at $' obj[itemPrice];
-      if (cart.length > 1) {
+      if( (cart.length > 1)) {
         cart_str += ', ';
       }
       if 
