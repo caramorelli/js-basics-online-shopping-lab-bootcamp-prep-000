b01197bf7b4a615f8d6d75a770e80138378a1416
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 02:54:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ function removeFromCart(item) {
     if (item === obj.itemName) {
       cart.splice(i, 1);
     }
-    else if (i === cart.length - 1) {
+    else {
+      if (i === cart.length)
       return 'That item is not in your cart.';
     }
   }
