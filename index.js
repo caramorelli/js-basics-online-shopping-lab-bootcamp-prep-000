97bfe4837e3f638f07ca5318d927a28b023199fc
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 02:51:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     var obj = cart[i];
     if (item === obj.itemName) {
-      return cart.split(0, i) + cart.
+      return cart[0...i] + cart.split()
     }
     else if (i === cart.length - 1) {
       return 'That item is not in your cart.';
