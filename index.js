c168699478569e9bfeb452843732d7c48c1ffef3
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 02:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ function viewCart() {
 
 function total() {
   // write your code here
-  var total_amount = 0
+  var total_amount = 0;
+  for (var i = 0; i < cart.length; i++) {
+    
+  }
 }
 
 function removeFromCart(item) {
